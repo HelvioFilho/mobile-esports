5299fef559e5f5f1c17cd6b2b3325a1339e6e6fa
Hélvio Filho <helviosvf@gmail.com>
2022-09-18 13:58:01 -0300

    Chore: defining the types of routes

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      Game: {
+        id: string;
+        title: string;
+        bannerUrl: string;
+      }
+    }
+  }
+}
