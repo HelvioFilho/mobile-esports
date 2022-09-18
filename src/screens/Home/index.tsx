25a4bc7ab954a80892650c7dffb4930645a64821
Hélvio Filho <helviosvf@gmail.com>
2022-09-18 14:00:47 -0300

    Feat: calling game route and sending data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,9 +7,18 @@ import { Container, Logo } from './styles';
 
 import { FlatList } from 'react-native';
 import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
   const [game, setGame] = useState<GameCardProps[]>([]);
+
+  const { navigate } = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigate('Game', { id, title, bannerUrl });
+  }
+
   useEffect(() => {
     fetch('http://192.168.100.107:3333/games')
       .then(response => response.json())
@@ -17,26 +26,29 @@ export function Home() {
   }, []);
 
   return (
-    <Container>
-      <Logo
-        source={logoImg}
-      />
-      <Heading
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."
-      />
-      <FlatList
-        data={game}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard
-            data={item}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingLeft: 32, paddingRight: 64 }}
-      />
-    </Container>
+    <Background>
+      <Container>
+        <Logo
+          source={logoImg}
+        />
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+        <FlatList
+          data={game}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+              onPress={() => handleOpenGame(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingLeft: 32, paddingRight: 64 }}
+        />
+      </Container>
+    </Background>
   );
 }
